31ca44e4e5d695bda6a1384a841a542b6896705f
[bymagka] <[bymagka@bk.ru]>
2020-10-31 01:31:09 +0300

    Task2 Ready

---
+++ Homework_Lesson2_TininA_Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_Lesson2_TininA_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Введите число, чье количество цифр необходимо посчитать");
+            int Number = int.Parse(Console.ReadLine());
+            int count = 0;
+            while(Number != 0)
+            {
+                count++;
+                Number = Number / 10;
+            }
+
+            Console.WriteLine($"Количество цифр введённого числа: {count}");
+            Console.ReadLine();
+        }
+    }
+}
