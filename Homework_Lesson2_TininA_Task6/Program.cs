d4361e38a7a6cec2da4a11687e56becc97d3a93e
[bymagka] <[bymagka@bk.ru]>
2020-10-31 12:40:24 +0300

    Task 6 is ready

---
+++ Homework_Lesson2_TininA_Task6/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * TininA
+ * *Написать программу подсчета количества «хороших» чисел в диапазоне от 1 до 1 000 000 000. «Хорошим» называется число, которое делится на сумму своих цифр. Реализовать подсчёт времени выполнения программы, используя структуру DateTime.
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_Lesson2_TininA_Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Запускаем расчет \"правильных\" чисел в диапазоне от 1 до 1 000 000! Это займет какое то время");
+
+            DateTime StartDate = DateTime.Now;
+       
+            int count = 0;
+            for (int i = 1; i <= 1000000; i++)
+            {
+                if (i % SumOfDigits(i) == 0) count++;
+            }
+            Console.WriteLine($"Количество \"правильных\" чисел равно {count}");
+            Console.WriteLine($"Выполнение расчета заняло {(DateTime.Now - StartDate).TotalSeconds} секунд");
+            Console.ReadLine();
+        }
+
+    
+        public static int SumOfDigits(int number)
+        {
+            int sum = 0;
+            while (number != 0)
+            {
+                sum += number % 10;
+                number = number / 10;
+            }
+            return sum;
+        }
+    }
+
+}
