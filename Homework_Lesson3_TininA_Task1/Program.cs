34480027f81989308fbb3bc6d1cde2ef3e63f7e9
[bymagka] <[bymagka@bk.ru]>
2020-11-02 11:52:42 +0300

    commit

---
+++ Homework_Lesson3_TininA_Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_Lesson3_TininA_Task1
+{
+    partial class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
