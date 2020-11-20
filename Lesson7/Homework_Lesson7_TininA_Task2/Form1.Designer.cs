f444b347e3b1e384c3d1dc373a349149eee0176f
bymagka <bymagka@bk.ru>
2020-11-20 23:24:14 +0300

    Task2

---
+++ Lesson7/Homework_Lesson7_TininA_Task2/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace Homework_Lesson7_TininA_Task2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.StepsCount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(71, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 59);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Загадать число";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(71, 118);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(151, 61);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Отгадать число";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 224);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Количество ходов:";
+            // 
+            // StepsCount
+            // 
+            this.StepsCount.AutoSize = true;
+            this.StepsCount.Location = new System.Drawing.Point(171, 224);
+            this.StepsCount.Name = "StepsCount";
+            this.StepsCount.Size = new System.Drawing.Size(0, 20);
+            this.StepsCount.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(271, 293);
+            this.Controls.Add(this.StepsCount);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Угадай число!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.Label StepsCount;
+    }
+}
+
