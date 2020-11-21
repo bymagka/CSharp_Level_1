515d5dc3f9bf6b9d01337897c14535e9bef00401
bymagka <bymagka@bk.ru>
2020-11-21 21:05:36 +0300

    Task1

---
+++ Lesson8/Homework_Lesson8_TininA/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Tinin A
+ * С помощью рефлексии выведите все свойства структуры DateTime
+ */
+namespace Homework_Lesson8_TininA_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var properties = typeof(DateTime).GetProperties();
+            foreach (var item in properties)
+            {
+                Console.WriteLine(item.Name);
+            }
+            Console.ReadLine();
+        }
+    }
+}
