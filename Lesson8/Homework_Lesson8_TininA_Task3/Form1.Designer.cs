027751a04589bf3fcf3741fdecb5f3045ee36d33
bymagka <bymagka@bk.ru>
2020-11-23 23:40:39 +0300

    Task 3

--- Lesson8/Homework_Lesson8_TininA_Task3/Form1.Designer.cs
+++ Lesson8/Homework_Lesson8_TininA_Task3/Form1.Designer.cs
@@ -31,136 +31,174 @@ namespace Homework_Lesson8_TininA_Task3
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.miNew = new System.Windows.Forms.ToolStripMenuItem();
+            this.miOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.miSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.miExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.tbQuestion = new System.Windows.Forms.TextBox();
             this.btnAdd = new System.Windows.Forms.Button();
             this.btnDelete = new System.Windows.Forms.Button();
             this.btnSaveQuest = new System.Windows.Forms.Button();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.nudNumber = new System.Windows.Forms.NumericUpDown();
+            this.cboxTrue = new System.Windows.Forms.CheckBox();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnInfo = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumber)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
+            this.menuStrip1.BackColor = System.Drawing.Color.Silver;
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fileToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(789, 36);
+            this.menuStrip1.Size = new System.Drawing.Size(789, 33);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // fileToolStripMenuItem
             // 
             this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.newToolStripMenuItem,
-            this.openToolStripMenuItem,
-            this.saveToolStripMenuItem,
-            this.exitToolStripMenuItem});
+            this.miNew,
+            this.miOpen,
+            this.miSave,
+            this.saveAsToolStripMenuItem,
+            this.miExit});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(54, 30);
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(54, 32);
             this.fileToolStripMenuItem.Text = "File";
             // 
-            // newToolStripMenuItem
+            // miNew
             // 
-            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
-            this.newToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
-            this.newToolStripMenuItem.Text = "New";
+            this.miNew.Name = "miNew";
+            this.miNew.Size = new System.Drawing.Size(270, 34);
+            this.miNew.Text = "New";
+            this.miNew.Click += new System.EventHandler(this.miNew_Click);
             // 
-            // openToolStripMenuItem
+            // miOpen
             // 
-            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
-            this.openToolStripMenuItem.Text = "Open";
+            this.miOpen.Name = "miOpen";
+            this.miOpen.Size = new System.Drawing.Size(270, 34);
+            this.miOpen.Text = "Open";
+            this.miOpen.Click += new System.EventHandler(this.miOpen_Click);
             // 
-            // saveToolStripMenuItem
+            // miSave
             // 
-            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
-            this.saveToolStripMenuItem.Text = "Save";
+            this.miSave.Name = "miSave";
+            this.miSave.Size = new System.Drawing.Size(270, 34);
+            this.miSave.Text = "Save";
+            this.miSave.Click += new System.EventHandler(this.miSave_Click);
             // 
-            // exitToolStripMenuItem
+            // miExit
             // 
-            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
-            this.exitToolStripMenuItem.Text = "Exit";
+            this.miExit.Name = "miExit";
+            this.miExit.Size = new System.Drawing.Size(270, 34);
+            this.miExit.Text = "Exit";
+            this.miExit.Click += new System.EventHandler(this.miExit_Click);
             // 
-            // textBox1
+            // tbQuestion
             // 
-            this.textBox1.Location = new System.Drawing.Point(0, 36);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(789, 220);
-            this.textBox1.TabIndex = 2;
+            this.tbQuestion.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.tbQuestion.Location = new System.Drawing.Point(0, 116);
+            this.tbQuestion.Multiline = true;
+            this.tbQuestion.Name = "tbQuestion";
+            this.tbQuestion.Size = new System.Drawing.Size(789, 140);
+            this.tbQuestion.TabIndex = 2;
             // 
             // btnAdd
             // 
-            this.btnAdd.Location = new System.Drawing.Point(12, 262);
+            this.btnAdd.BackColor = System.Drawing.Color.Green;
+            this.btnAdd.ForeColor = System.Drawing.Color.Yellow;
+            this.btnAdd.Location = new System.Drawing.Point(12, 72);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(119, 30);
+            this.btnAdd.Size = new System.Drawing.Size(119, 38);
             this.btnAdd.TabIndex = 3;
             this.btnAdd.Text = "Добавить";
-            this.btnAdd.UseVisualStyleBackColor = true;
-            this.btnAdd.Click += new System.EventHandler(this.button1_Click);
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // btnDelete
             // 
-            this.btnDelete.Location = new System.Drawing.Point(137, 262);
+            this.btnDelete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnDelete.ForeColor = System.Drawing.Color.Black;
+            this.btnDelete.Location = new System.Drawing.Point(165, 72);
             this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(147, 30);
+            this.btnDelete.Size = new System.Drawing.Size(147, 38);
             this.btnDelete.TabIndex = 4;
             this.btnDelete.Text = "Удалить";
-            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
             // btnSaveQuest
             // 
-            this.btnSaveQuest.Location = new System.Drawing.Point(300, 262);
+            this.btnSaveQuest.BackColor = System.Drawing.SystemColors.Info;
+            this.btnSaveQuest.Location = new System.Drawing.Point(330, 72);
             this.btnSaveQuest.Name = "btnSaveQuest";
-            this.btnSaveQuest.Size = new System.Drawing.Size(130, 30);
+            this.btnSaveQuest.Size = new System.Drawing.Size(130, 38);
             this.btnSaveQuest.TabIndex = 5;
             this.btnSaveQuest.Text = "Сохранить";
-            this.btnSaveQuest.UseVisualStyleBackColor = true;
+            this.btnSaveQuest.UseVisualStyleBackColor = false;
+            this.btnSaveQuest.Click += new System.EventHandler(this.btnSaveQuest_Click);
             // 
-            // numericUpDown1
+            // nudNumber
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(471, 266);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(47, 26);
-            this.numericUpDown1.TabIndex = 6;
+            this.nudNumber.Location = new System.Drawing.Point(466, 85);
+            this.nudNumber.Name = "nudNumber";
+            this.nudNumber.Size = new System.Drawing.Size(47, 26);
+            this.nudNumber.TabIndex = 6;
+            this.nudNumber.Click += new System.EventHandler(this.nudNumber_ValueChanged);
             // 
-            // checkBox1
+            // cboxTrue
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(525, 268);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(94, 24);
-            this.checkBox1.TabIndex = 7;
-            this.checkBox1.Text = "Правда";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.cboxTrue.AutoSize = true;
+            this.cboxTrue.Location = new System.Drawing.Point(533, 86);
+            this.cboxTrue.Name = "cboxTrue";
+            this.cboxTrue.Size = new System.Drawing.Size(94, 24);
+            this.cboxTrue.TabIndex = 7;
+            this.cboxTrue.Text = "Правда";
+            this.cboxTrue.UseVisualStyleBackColor = true;
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.saveAsToolStripMenuItem.Text = "Save as";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // btnInfo
+            // 
+            this.btnInfo.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.btnInfo.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnInfo.Location = new System.Drawing.Point(571, 7);
+            this.btnInfo.Name = "btnInfo";
+            this.btnInfo.Size = new System.Drawing.Size(175, 38);
+            this.btnInfo.TabIndex = 8;
+            this.btnInfo.Text = "О программе";
+            this.btnInfo.UseVisualStyleBackColor = false;
+            this.btnInfo.Click += new System.EventHandler(this.btnInfo_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
             this.ClientSize = new System.Drawing.Size(789, 304);
-            this.Controls.Add(this.checkBox1);
-            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.btnInfo);
+            this.Controls.Add(this.cboxTrue);
+            this.Controls.Add(this.nudNumber);
             this.Controls.Add(this.btnSaveQuest);
             this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.btnAdd);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tbQuestion);
             this.Controls.Add(this.menuStrip1);
             this.Name = "Form1";
             this.Text = "Верю - не верю";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumber)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -170,16 +208,18 @@ namespace Homework_Lesson8_TininA_Task3
 
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolStripMenuItem miNew;
+        private System.Windows.Forms.ToolStripMenuItem miOpen;
+        private System.Windows.Forms.ToolStripMenuItem miSave;
+        private System.Windows.Forms.ToolStripMenuItem miExit;
+        private System.Windows.Forms.TextBox tbQuestion;
         private System.Windows.Forms.Button btnAdd;
         private System.Windows.Forms.Button btnDelete;
         private System.Windows.Forms.Button btnSaveQuest;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
-        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.NumericUpDown nudNumber;
+        private System.Windows.Forms.CheckBox cboxTrue;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.Button btnInfo;
     }
 }
 
--- Lesson8/Homework_Lesson8_TininA_Task3/Form1.cs
+++ Lesson8/Homework_Lesson8_TininA_Task3/Form1.cs
@@ -1,25 +1,121 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+
+/*
+ * TininA
+ * 
+ * 
+ * а) Создать приложение, показанное на уроке, добавив в него защиту от возможных ошибок (не создана база данных, обращение к несуществующему вопросу, открытие слишком большого файла и т.д.).
+   б) Изменить интерфейс программы, увеличив шрифт, поменяв цвет элементов и добавив другие «косметические» улучшения на свое усмотрение.
+   в) Добавить в приложение меню «О программе» с информацией о программе (автор, версия, авторские права и др.).
+   г)* Добавить пункт меню Save As, в котором можно выбрать имя для сохранения базы данных (элемент SaveFileDialog).
+   д) Добавить в приложение сообщение с предупреждением при попытке удалить вопрос.
+ * 
+ */
+
 
 namespace Homework_Lesson8_TininA_Task3
 {
     public partial class Form1 : Form
     {
+        // База данных с вопросами
+        TrueFalse database;
         public Form1()
         {
             InitializeComponent();
         }
-
-        private void button1_Click(object sender, EventArgs e)
+        // Обработчик пункта меню Exit
+        private void miExit_Click(object sender, EventArgs e)
         {
+            this.Close();
+        }
+        // Обработчик пункта меню New
+        private void miNew_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                database = new TrueFalse(sfd.FileName);
+                database.Save();
+                nudNumber.Minimum = 1;
+                nudNumber.Maximum = 1;
+                nudNumber.Value = 1;
+            };
+        }
+        // Обработчик события изменения значения numericUpDown
+        private void nudNumber_ValueChanged(object sender, EventArgs e)
+        {
+            tbQuestion.Text = database[(int)nudNumber.Value - 1].Text;
+            cboxTrue.Checked = database[(int)nudNumber.Value - 1].TrueFalse;
+        }
+        // Обработчик кнопки Добавить
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (database == null)
+            {
+                MessageBox.Show("Создайте новую базу данных", "Сообщение");
+                return;
+            }
+            database.Add(tbQuestion.Text, cboxTrue.Checked);
+            nudNumber.Maximum = database.Count;
+            nudNumber.Value = database.Count;
+        }
+        // Обработчик кнопки Удалить
 
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            // а) Создать приложение, показанное на уроке, добавив в него защиту от возможных ошибок (не создана база данных, обращение к несуществующему вопросу, открытие слишком большого файла и т.д.).
+            if (nudNumber.Maximum == 1 || database == null) return;
+
+            // д) Добавить в приложение сообщение с предупреждением при попытке удалить вопрос.
+            MessageBox.Show($"Вопрос {tbQuestion.Text} будет удален");
+
+            database.Remove((int)nudNumber.Value);
+            nudNumber.Maximum--;
+            if (nudNumber.Value > 1) nudNumber.Value = nudNumber.Value;
+        }
+        // Обработчик пункта меню Save
+        private void miSave_Click(object sender, EventArgs e)
+        {
+            if (database != null) database.Save();
+            // а) Создать приложение, показанное на уроке, добавив в него защиту от возможных ошибок (не создана база данных, обращение к несуществующему вопросу, открытие слишком большого файла и т.д.).
+            else MessageBox.Show("База данных не создана");
+        }
+        // Обработчик пункта меню Open
+        private void miOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                database = new TrueFalse(ofd.FileName);
+                database.Load();
+                nudNumber.Minimum = 1;
+                nudNumber.Maximum = database.Count;
+                nudNumber.Value = 1;
+            }
+        }
+        // Обработчик кнопки Сохранить (вопрос)
+        private void btnSaveQuest_Click(object sender, EventArgs e)
+        {
+            database[(int)nudNumber.Value - 1].Text = tbQuestion.Text;
+            database[(int)nudNumber.Value - 1].TrueFalse = cboxTrue.Checked;
+        }
+
+        // г)* Добавить пункт меню Save As, в котором можно выбрать имя для сохранения базы данных (элемент SaveFileDialog).
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveAsFileDialog = new SaveFileDialog();
+            if(saveAsFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                database.FileName = saveAsFileDialog.FileName;
+                database.Save();
+            }
+        }
+
+        //в) Добавить в приложение меню «О программе» с информацией о программе (автор, версия, авторские права и др.).
+        private void btnInfo_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Автор: Тинин А. \nВерсия:1.0 \nБлагодаронсти: GeekBrains");
         }
     }
 }
